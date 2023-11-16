4577a1e1242d58d0c68c1eb5cb326d09f102c6a8
uuuuj <yjth4436@gmail.com>
2023-11-16 17:01:11 +0900

    feat: ec2 server version code. IAM 권한 사용 -> Access key 및 Region 설정 제거

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -2,15 +2,26 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import * as ffmpeg from 'fluent-ffmpeg';
+import * as fs from 'fs';
+import * as path from 'path';
+
 @Injectable()
 export class UploadService {
-    private readonly s3client = new S3Client({ region: this.configService.getOrThrow('AWS_S3_REGION'),
-});
-    constructor(private readonly configService: ConfigService) {}
+    private readonly s3client = new S3Client({});
+    // constructor(private readonly configService: ConfigService) {}
 
     async upload(fileName: string, file: Buffer) {
-        //HLS 인코딩 로직
-        await this.encodeToHLS(file, fileName);
+        //서버 공간에 동영상 임시 저장
+        const tempFilePath = path.join("home/ubuntu/temp", fileName);
+        //tempFilePath에 file을 저장
+        fs.writeFileSync(tempFilePath, file);
+        console.log('File saved to ${tempFilePath}');
+        //HLS 인코딩
+        await this.encodeToHLS(file, fileName, tempFilePath);
+
+        //인코딩이 끝나면, 임시 저장해둔 파일 삭제
+        fs.unlinkSync(tempFilePath);
+        console.log('File deleted from ${tempFilePath}'); 
 
         await this.s3client.send(
             //인코딩된 파일을 S3에 업로드
@@ -22,20 +33,18 @@ export class UploadService {
         )
     }
 
-    private async encodeToHLS(file: Buffer, fileName: string): Promise<void> {
-        const fs = require('fs');
-        const path = require('path');
+    private async encodeToHLS(file: Buffer, fileName: string, tempFilePath: string): Promise<void> {
         //fileName에서 확장자 제거
         const baseName = path.basename(fileName, path.extname(fileName));
 
         //HLS 파일을 저장할 새로운 폴더경로
-        const hlsFolderPath = path.join("C:/HLS_video", baseName);
+        const hlsFolderPath = path.join("home/ubuntu/video_test", baseName);
         if (!fs.existsSync(hlsFolderPath)) {
             fs.mkdirSync(hlsFolderPath, { recursive: true });
         }
         return new Promise((resolve, reject) => {
             //임시 파일 생성
-            const tempFilePath = "C:/Programming/SWJungle_07/나만무/test동영상/file.mp4";   //임시 파일 경로 -> 로컬 환경 테스트 용이라 실제 파일 경로
+            // const tempFilePath = "C:/Programming/SWJungle_07/나만무/test동영상/file.mp4";   //임시 파일 경로 -> 로컬 환경 테스트 용이라 실제 파일 경로
             fs.writeFileSync(tempFilePath, file);   //임시 파일 생성
             ffmpeg(tempFilePath)
                 .output(path.join(hlsFolderPath, 'video.m3u8')) 
@@ -54,8 +63,5 @@ export class UploadService {
                 }) 
                 .run(); 
         });//FFMPEG를 사용하여 HLS로 인코딩
-        
-        // 인코딩 된 파일을 임시 경로에 저장
-        // 인코딩이 완료되면, 해당 파일들을 S3에 업로드
     }
 }
