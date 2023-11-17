98782076af053d7de8819109314e3fa3d3b0289c
uuuuj <yjth4436@gmail.com>
2023-11-18 01:35:18 +0900

    modify: app.module에 country.module추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { UploadModule } from './upload/upload.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm'; 
 import { typeOrmConfig } from './configs/typeorm.config';
+import { ScheduleModule } from '@nestjs/schedule';
+
+import { CountryModule } from './country/country.module';
+import { CountryEntity } from './country/country.entity';
+import { CountryMappings } from './country/script_init/country-table.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    UploadModule, ConfigModule.forRoot({ isGlobal: true })],
+    ScheduleModule.forRoot(),
+    UploadModule, CountryModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
