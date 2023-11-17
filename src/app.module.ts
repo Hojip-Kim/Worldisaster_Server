8d02140a6cc720559c5696f8aae0bd6a0126507c
uuuuj <yjth4436@gmail.com>
2023-11-18 02:30:02 +0900

    feat: disasters  관련 파일 업로드

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { ScheduleModule } from '@nestjs/schedule';
 
+import { DisastersModule } from './disasters/disasters.module';
+import { DisastersList } from './disasters/disasters-list.entity';
+import { DisastersDetailEntity } from './disasters/disasters-detail.entity';
+
 import { CountryModule } from './country/country.module';
 import { CountryEntity } from './country/country.entity';
 import { CountryMappings } from './country/script_init/country-table.entity';
@@ -13,6 +17,6 @@ import { CountryMappings } from './country/script_init/country-table.entity';
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     ScheduleModule.forRoot(),
-    UploadModule, CountryModule, ConfigModule.forRoot({ isGlobal: true })],
+    UploadModule, CountryModule, DisastersModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
---
+++ src/disasters/disasters.module.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common';
+import { DisastersService } from './disasters.service';
+import { DisastersDetailEntity } from './disasters-detail.entity';
+
+@Controller('disasters')
+export class DisastersController {
+    constructor(private readonly disastersService: DisastersService) { }
+
+    /* Debugger API (to-delete) */
+
+    @Get('/')
+    async debug_get_all_disasters(): Promise<DisastersDetailEntity[]> {
+        console.log('API : GET call made to fetch all disasters');
+        return await this.disastersService.getAllDisasters();
+    }
+
+    @Get('force')
+    async debug_force_refresh(): Promise<{ success: boolean, message: string }> {
+        console.log('API : GET call made to force refresh disasters DB (Debug)');
+        return await this.disastersService.fetchAndCompareCount();
+    }
+}
