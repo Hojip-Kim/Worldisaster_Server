e851763fa931ba615116a16658521ea6f48c42cd
uuuuj <yjth4436@gmail.com>
2023-11-26 02:27:38 +0900

    modify: debug

--- src/disasters/disasters.service.ts
+++ src/disasters/disasters.service.ts
@@ -522,7 +522,7 @@ export class DisastersService {
             access_key: '5057f1372fdc2004d02af923fdeff472', // 여기에 실제 액세스 키를 입력하세요
             category : 'general',
             sort: 'published_desc',
-            keywords: `${dType}, ${dCountry}`,
+            keywords: `${dType},${dCountry}`,
             date: `${dDate}`,
             limit: 1,
         });
