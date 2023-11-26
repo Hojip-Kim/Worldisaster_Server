bfbe15cade97f35f91ced388c83e66bc7fa4f47d
uuuuj <yjth4436@gmail.com>
2023-11-26 16:12:55 +0900

    modify: 임시로 port 3001 -> 3000

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
-  await app.listen(3001, "0.0.0.0");
+  // await app.listen(3001, "0.0.0.0");
+  await app.listen(3000);
 }
 bootstrap();
