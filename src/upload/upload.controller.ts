5958ae804c3c8f6d145b12f6017995b95c428870
uuuuj <yjth4436@gmail.com>
2023-11-27 14:08:17 +0900

    modify: html 코드 주석처리

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -27,38 +27,38 @@ export class UploadController {
             console.log('video_url is null');
             return res.status(404).send('Video not found');
         }
-        const htmlContent = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <title>Video Test</title>
-            <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
-            <script src="https://unpkg.com/video.js/dist/video.js"></script>
-            <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/hls.js@canary"></script>
-        </head>
-        <body>
-            <video id="example-video" width="960" height="540" class="video-js vjs-default-skin" controls>
-                <source src="${video_url}" type="application/x-mpegURL">
-            </video>
-            <script>
-                var player = videojs('example-video');
-                if (Hls.isSupported()) {
-                    var hls = new Hls();
-                    hls.loadSource('${video_url}');
-                    hls.attachMedia(player.el());
-                    hls.on(Hls.Events.MANIFEST_PARSED, function() {
-                        player.play();
-                    });
-                } else if (player.tech().canPlayType('application/vnd.apple.mpegurl')) {
-                    player.src('${video_url}');
-                    player.play();
-                }
-            </script>
-        </body>
-        </html>`;
-        res.status(200).send(htmlContent);
+        // const htmlContent = `
+        // <!DOCTYPE html>
+        // <html lang="en">
+        // <head>
+        //     <meta charset="UTF-8">
+        //     <title>Video Test</title>
+        //     <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
+        //     <script src="https://unpkg.com/video.js/dist/video.js"></script>
+        //     <script src="https://unpkg.com/videojs-contrib-hls/dist/videojs-contrib-hls.js"></script>
+        //     <script src="https://cdn.jsdelivr.net/npm/hls.js@canary"></script>
+        // </head>
+        // <body>
+        //     <video id="example-video" width="960" height="540" class="video-js vjs-default-skin" controls>
+        //         <source src="${video_url}" type="application/x-mpegURL">
+        //     </video>
+        //     <script>
+        //         var player = videojs('example-video');
+        //         if (Hls.isSupported()) {
+        //             var hls = new Hls();
+        //             hls.loadSource('${video_url}');
+        //             hls.attachMedia(player.el());
+        //             hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        //                 player.play();
+        //             });
+        //         } else if (player.tech().canPlayType('application/vnd.apple.mpegurl')) {
+        //             player.src('${video_url}');
+        //             player.play();
+        //         }
+        //     </script>
+        // </body>
+        // </html>`;
+        // res.status(200).send(htmlContent);
     }
     
 }
